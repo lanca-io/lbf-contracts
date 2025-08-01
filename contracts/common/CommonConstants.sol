17532f5c64252bcd8d4e59cc8a443b1d3df10629
Roman Yarlykov <roman.yarlykov@concero.io>
2025-08-01 11:42:36 +0500

    add: lanca bridge fee

---
+++ contracts/common/CommonConstants.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+
+library CommonConstants {
+    uint256 internal constant BPS_DENOMINATOR = 10_000;
+    uint256 internal constant REBALANCER_PREMIUM_BPS = 1;
+    uint256 internal constant LP_PREMIUM_BPS = 1;
+    uint256 internal constant LANCA_BRIDGE_PREMIUM_BPS = 5;
+}
